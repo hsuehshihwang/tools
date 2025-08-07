26f47c94496bcb5a95c229292691596a52c3d06d
Ralph Wang <enhanceralph@gmail.com>
2025-08-07 15:02:06 +0800

    Update

--- gen.sh
+++ gen.sh
@@ -3,4 +3,4 @@ file=${1:-sample.md}
 output=${2:-${file%.md}.html}
 # echo file=$file, output=$output
 # npx markmap-cli@0.14 --no-open ${file}
-[ -f $file ] && markmap --no-open ${file}
+[ -f $file ] && markmap --no-open ${file} -o ${output}
